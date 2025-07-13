d075c602d15457cc68e3c56d00717c7920c9f2ee
Jman <croatonian@gmail.com>
2025-07-13 18:06:34 +0300

    2

--- hr_employee_firstname/models/hr_employee_base.py
+++ hr_employee_firstname/models/hr_employee_base.py
@@ -4,5 +4,6 @@ from odoo import fields, models
 class HrEmployeeBase(models.AbstractModel):
     _inherit = "hr.employee.base"
 
-    firstname = fields.Char()
-    lastname = fields.Char()
+    firstname = fields.Char("Ім'я")
+    lastname = fields.Char("Прізвище")
+    lastname2 = fields.Char("По батькові")
